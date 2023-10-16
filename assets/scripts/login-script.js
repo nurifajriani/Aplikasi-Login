43346c48a186f2c12a7c72484452bb2a19468bb4
Nuri Fajriani <106643985+nurifajriani@users.noreply.github.com>
2023-10-16 12:23:01 +0700

    Update login-script.js
    mengganti komentar dengan format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,21 +5,53 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/**@module Login-Script*/
+/**
+*Membuat variabel loginFormElement untuk tampilan form.
+*@constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+/**
+*Membuat variabel inputEmailElement untuk input email.
+*@constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+/**
+*Membuat variabel inputPasswordElement untuk input password.
+*@constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/**
+*Membuat variabel expectedEmail untuk menyimpan email sementara.
+*@constant {string}
+*/
 const expectedEmail = 'admin@dicoding.com';
+/**
+*Membuat variabel expectedPassword untuk menyimpan password sementara.
+*@constant {string}
+*/
 const expectedPassword = 'superpassword';
-
+/*Comment : menambahkan aksi klik pada button*/
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  /**
+  *Membuat variabel email untuk menyimpan nilai email yang didapat saat button di tekan.
+  *@constant {string}
+  */
   const email = inputEmailElement.value;
+  /**
+  *Membuat variabel password untuk menyimpan nilai password yang didapat saat button di tekan.
+  *@constant {string}
+  */
   const password = inputPasswordElement.value;
+  /* Comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */
   if (email == expectedEmail && password == expectedPassword) {
+   /* Comment : Jika sesuai maka program akan berpindah ke halaman home. */
     goToHome();
   } else {
+    /* Comment : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
     showPopUp();
   }
 });
